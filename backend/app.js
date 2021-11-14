d1346f2e2a0cabee00600e61289e0047530319ea
Aditya Prakash <aditya.eth@gmail.com>
2021-11-14 12:13:08 +0530

    moved dummy data out of middleware function

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,20 @@ app.use(cors({
   origin: '*',
   credentials: true
 }));
+
+const posts = [
+  {
+    id: "fadf12421l",
+    title: "First server-side post",
+    content: "This is coming from the server"
+  },
+  {
+    id: "ksajflaj132",
+    title: "Second server-side post",
+    content: "This is coming from the server!"
+  }
+];
+
 app.post("/api/posts", (req, res, next) => {
   const post = req.body;
   console.log(post);
@@ -18,18 +32,6 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
   res.status(200).json({
     message: "Posts fetched successfully!",
     posts: posts
